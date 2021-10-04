a72f5e34085a2ad5773ef790db6f3260a5b659ca
Alon Loeffler <50392552+aloe8475@users.noreply.github.com>
2021-10-04 18:06:55 +1100

    Update conv2linear.py

--- conv2linear.py
+++ conv2linear.py
@@ -1,4 +1,4 @@
-  def flattenConv(self,IMG_SIZE,kernel_size,padding,stride,num_layers=3,num_nodes=[]):
+  def conv2linear(self,IMG_SIZE,kernel_size,padding,stride,num_layers=3,num_nodes=[]):
         for layer in range(num_layers):
             if layer == 0:
                 layershape=np.hstack((int(((IMG_SIZE[0]-kernel_size+2*padding)/stride)+1),(int(((IMG_SIZE[1]-kernel_size+2*padding)/stride)+1))))
