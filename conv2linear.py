f8e98e0f74e17039272eca4582b65baaa4a0d994
Alon Loeffler <50392552+aloe8475@users.noreply.github.com>
2021-10-04 17:57:29 +1100

    Create conv2linear.py

---
+++ conv2linear.py
@@ -0,0 +1,9 @@
+  def flattenConv(self,IMG_SIZE,kernel_size,padding,stride,num_layers=3,num_nodes=[]):
+        for layer in range(num_layers):
+            if layer == 0:
+                layershape=np.hstack((int(((IMG_SIZE[0]-kernel_size+2*padding)/stride)+1),(int(((IMG_SIZE[1]-kernel_size+2*padding)/stride)+1))))
+                poolshape=np.hstack(((layershape/2),num_nodes[layer])).astype(int)
+            else:
+                layershape=np.hstack((int(((poolshape[0]-kernel_size+2*padding)/stride)+1),(int(((poolshape[1]-kernel_size+2*padding)/stride)+1))))
+                poolshape=np.hstack(((layershape/2),num_nodes[layer])).astype(int)
+        return np.prod(poolshape)
