63ddff18c97a15ce4fec01d72b27253744f51c41
Alon Loeffler <50392552+aloe8475@users.noreply.github.com>
2022-06-17 14:51:05 +1000

    Update conv2linear.py

--- conv2linear.py
+++ conv2linear.py
@@ -2,6 +2,8 @@
         for layer in range(num_layers):
             if layer == 0: #if this is the first layer, we want the image size
                 layershape=np.hstack((int(((IMG_SIZE[0]-kernel_size+2*padding)/stride)+1),(int(((IMG_SIZE[1]-kernel_size+2*padding)/stride)+1))))
-            else: #if this is layer 2+, we want the previous layer size 
-                layershape=np.hstack((int(((layershape[0]-kernel_size+2*padding)/stride)+1),(int(((poolshape[1]-kernel_size+2*padding)/stride)+1))))
+                poolshape=np.hstack((int(((layershape[0]-pool_size+2*pool_padding)/pool_stride)+1),(int(((layershape[1]-pool_size+2*pool_padding)/pool_stride)+1)))).astype(int)
+            else: #if this is layer 2+, we want the previous output pool layer size 
+                layershape=np.hstack((int(((poolshape[0]-kernel_size+2*padding)/stride)+1),(int(((poolshape[1]-kernel_size+2*padding)/stride)+1))))
+                poolshape=np.hstack((int(((layershape[0]-pool_size+2*pool_padding)/pool_stride)+1),(int(((layershape[1]-pool_size+2*pool_padding)/pool_stride)+1)))).astype(int)
         return np.prod(layershape) 
